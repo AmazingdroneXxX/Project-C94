a37cabcaf76f4de86996e32dbd444a733666d104
AmazingdroneXxX <132356787+AmazingdroneXxX@users.noreply.github.com>
2024-04-02 19:02:31 +0700

    Add files via upload

---
+++ letschat_room.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAUxPqEycvpodi1N88ssftCCRCm36S3iaw",
+    authDomain: "projectletschat-cb8b0.firebaseapp.com",
+    projectId: "projectletschat-cb8b0",
+    databaseURL: "https://projectletschat-cb8b0-default-rtdb.firebaseio.com",
+    storageBucket: "projectletschat-cb8b0.appspot.com",
+    messagingSenderId: "102231053521",
+    appId: "1:102231053521:web:2ee9c42b23c3d80a5be1f0"
+  };
